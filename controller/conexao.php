aff92f4bff25079a17854dda5b68f6a4f5dd944a
DavidCosmo <davidcosmo2078@gmail.com>
2022-06-19 21:52:03 -0300

    first commit

---
+++ controller/conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "admin";
+$BD = "floricultura";
+
+global $pdo; 
+
+try{
+    $pdo = new PDO("mysql:dbname=".$BD."; host=".$servidor,$usuario,$senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "tudo certo :)";  
+}
+catch(PDOException $e){
+    echo "ERRO: ".$e->getMessage();
+    exit;
+}
+
+?>
